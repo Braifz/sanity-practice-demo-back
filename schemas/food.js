f6b263e66db5245a2f6c365ba73f5d6bd4d56716
Braifz <braian94lp@gmail.com>
2022-09-15 23:13:33 -0300

    first commit

---
+++ schemas/food.js
@@ -0,0 +1,17 @@
+export default {
+  name: "food",
+  type: "document",
+  title: "Food",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+    },
+  ],
+};
